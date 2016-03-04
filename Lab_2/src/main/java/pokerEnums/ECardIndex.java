9b7ec9a61e4711ed61429e04eb9f148f56bc8bf3
auziabbruzzesi <austinta@udel.edu>
2016-03-03 21:39:33 -0500

    filled out the ECardIndex enum

--- Lab_2/src/main/java/pokerEnums/ECardIndex.java
+++ Lab_2/src/main/java/pokerEnums/ECardIndex.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package pokerEnums;
 
 /**
@@ -9,5 +7,26 @@ package pokerEnums;
  */
 //to contain enums for to access the n'th card
 public enum ECardIndex {
+	FIRST(0),
+	SECOND(1),
+	THIRD(2),
+	FOURTH(3),
+	FIFTH(4),
+	SIXTH(5),
+	SEVENTH(6),
+	EIGHTH(7),
+	NINETH(8),
+	TENTH(9),
+	ELEVENTH(10);
+	//it goes up to 11
+	private int nthCard;
+	
+	public int getNthCard(){
+		return nthCard;
+	}
+	
+	private ECardIndex(int nthCard){
+		this.nthCard = nthCard;
+	}
 
 }
