73582c2c7d0a7865f4f1279d27582311eb91f945
auziabbruzzesi <austinta@udel.edu>
2016-03-05 02:13:59 -0500

    Junit Tests
    Test names for Deck(unfinished)
    Class for Hand tests (empty as of now)
    A temporary Class that generates a hand (unreliable, to be thrown out)

--- Lab_2/src/test/java/base/DeckTest.java
+++ Lab_2/src/test/java/base/DeckTest.java
@@ -1,8 +1,35 @@
 package base;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
 public class DeckTest {
-	//do i have to push this?
+
+	/*@Test
+	public void isDrawCard(){//This test passes with a dumby deck!
+		ArrayList<Card> testDeck = new ArrayList<Card>();
+		
+		TempDeck deckTestOne = new TempDeck(testDeck);
+		deckTestOne.add();
+		assertTrue(deckTestOne.draw() instanceof Card );*/
+	}
 	
-	//I'm going to push these changes to my team's branch..
+	
+	@Test
+	public void isCardRemoved(testDeck){//Check if the card drawn is actually removed
+	
+	}
+	
+	@Test 
+	public void isOverdrawException(testDeck){//See if overdrawing Deck creates exception
+		
+	}
+	
+	@Test
+	public void isNewDeckFull(testDeck){//See if card w/o any draws contains 52 cards (w/o Jokers)
 
 }
+
